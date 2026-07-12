67f07bb95b8b2197955c174bf1ffcdfb8639d9a2
Matt <williamsmatt1239@gmail.com>
2026-07-11 20:15:34 -0400

    Remove unnecessary commands from autostart

--- modules/autostart.lua
+++ modules/autostart.lua
@@ -2,8 +2,6 @@
 --- AUTOSTART -------
 ---------------------
 hl.on("hyprland.start", function()
-    hl.exec_cmd("nix run ~/nix-dots#lokiNoctalia")
-    hl.exec_cmd("hyprctl setcursor Adwaita 24") 
     hl.exec_cmd("noctalia")
     hl.exec_cmd("hyprctl setcursor Bibata-Modern-Ice 20")
 end)
